b8ed9a0a08da6e613a3e47955a9d8cdda0248dcb
Verpul <nicenick@mail.ru>
2021-09-14 16:20:34 +0300

    0.0.4 EDS page

---
+++ src/main/java/ru/mond51/workplace/controller/eds.java
@@ -0,0 +1,16 @@
+package ru.mond51.workplace.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/eds")
+public class eds {
+
+    @GetMapping
+    public String edsTable(Model model) {
+        return  "edsTable";
+    }
+}
