3fa8528dafa465fcddd2bb457b6e635c099e5e55
Verpul <nicenick@mail.ru>
2021-09-15 12:41:33 +0300

    0.0.8 upload test data to DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+delete from eds;
+insert into eds (id, active, begin_date, comment, expiration_date, owner, purpose)
+    values ('1', 1, '2002.12.12', 'Установить в кассе', '2021.12.12','Игнатова', 'СУФД');
+insert into eds (id, active, begin_date, comment, expiration_date, owner, purpose)
+    values ('2', 1, '2001.11.12', 'Установить в кассе и экономистам', '2022.02.04','Заплаткин Игорь Александрович', 'Госзакупки, СУФД');
+insert into eds (id, active, begin_date, comment, expiration_date, owner, purpose)
+    values ('3', 1, '2013.10.12', 'Установить в ОК и материльную часть', '2019.01.10','Бармина', 'Контур');
+insert into eds (id, active, begin_date, comment, expiration_date, owner, purpose)
+    values ('4', 0, '2020.04.08', 'Срок действия истек', '2025.05.12','Бармина', 'Контур');
