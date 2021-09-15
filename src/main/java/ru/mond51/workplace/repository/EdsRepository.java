cbe5cd58c47f4f056514e1f9f7ac917090bab9f9
Verpul <nicenick@mail.ru>
2021-09-15 10:34:39 +0300

    0.0.6 Eds repository

---
+++ src/main/java/ru/mond51/workplace/repository/EdsRepository.java
@@ -0,0 +1,7 @@
+package ru.mond51.workplace.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.mond51.workplace.model.Eds;
+
+public interface EdsRepository extends CrudRepository<Eds, Long> {
+}
