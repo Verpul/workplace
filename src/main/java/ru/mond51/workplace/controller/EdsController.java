8dbeefc2a1120e40f1076f6c6612aba309eeac49
Verpul <nicenick@mail.ru>
2021-09-15 11:47:12 +0300

    0.0.7 changed MySQL to MariaDB connect

--- src/main/java/ru/mond51/workplace/controller/EdsController.java
+++ src/main/java/ru/mond51/workplace/controller/EdsController.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mond51.workplace.repository.EdsRepository;
 
 @Controller
 @RequestMapping("/eds")
 public class EdsController {
 
+    private EdsRepository edsRepository;
+
+    public EdsController(EdsRepository edsRepository) {
+        this.edsRepository = edsRepository;
+    }
+
     @GetMapping
     public String edsTable(Model model) {
+        model.addAttribute("eds", edsRepository.findAll());
+
         return  "edsTable";
     }
 }
