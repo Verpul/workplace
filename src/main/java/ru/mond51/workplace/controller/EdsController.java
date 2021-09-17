0ea09a0b07422515ef8e085bc55abcf84cb2e66d
Verpul <nicenick@mail.ru>
2021-09-17 10:00:02 +0300

    0.0.12 Eds now can be deleted

--- src/main/java/ru/mond51/workplace/controller/EdsController.java
+++ src/main/java/ru/mond51/workplace/controller/EdsController.java
@@ -3,6 +3,7 @@ package ru.mond51.workplace.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.mond51.workplace.repository.EdsRepository;
 
@@ -21,5 +22,12 @@ public class EdsController {
         model.addAttribute("eds", edsRepository.findAllByOrderByActiveDescExpirationDateAsc());
 
         return  "eds/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        edsRepository.deleteById(id);
+
+        return "redirect:/eds";
     }
 }
